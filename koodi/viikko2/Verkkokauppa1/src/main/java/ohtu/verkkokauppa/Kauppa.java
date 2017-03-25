ece5e70b57c32f1c10751c24d3cc0d64dc7fa444
Andrea <happtur@users.noreply.github.com>
2017-03-25 17:32:05 +0200

    spring

--- koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -1,5 +1,9 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Kauppa {
 
     private VaInterface varasto;
@@ -8,6 +12,7 @@ public class Kauppa {
     private ViiInterface viitegeneraattori;
     private String kaupanTili;
     
+    @Autowired
     public Kauppa(VaInterface va, PaInterface pa, ViiInterface vi) {
         varasto = va;
         pankki = pa;
--- koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -1,12 +1,16 @@
 package ohtu.verkkokauppa;
 
 import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Varasto implements VaInterface {
     
     private KiInterface kirjanpito;
     private HashMap<Tuote, Integer> saldot;  
     
+    @Autowired
     public Varasto(KiInterface ki) {
         kirjanpito = ki;
         saldot = new HashMap<Tuote, Integer>();
